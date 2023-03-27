86d93f030a313e68deed800807e379890fe22b3b
Alexandr Rossovskiy <AlRoss@corp.com>
2023-03-27 13:58:45 +0300

    Capitals added

--- File1.py
+++ File1.py
@@ -2,3 +2,5 @@ def greet(name):
 	return f'Hello world! Hello {name}. We are glad to see you'
 	
 print(greet('Alex'))
+print("London is the capital of GB")
+print("Moscow is the capital of Russian Federation")
